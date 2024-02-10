923a2bc71de4f6bdf63d8cdd2da7b663212f24bf
shun <ledhouse2077@gmail.com>
2024-02-10 18:26:17 +0900

    6.3.3 バリデーション実行順序の設定 実装完了

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -41,7 +43,7 @@ public class SignupController {
 	//ユーザー登録処理
 	@PostMapping("/signup")
 	public String postSignup(Model model,Locale locale,
-			@ModelAttribute SignupForm form,
+			@ModelAttribute @Validated(GroupOrder.class) SignupForm form,
 			BindingResult bindingResult
 			) {
 		
