ea80dc5d75bbc59f24c04c97dfd0891a77a1ced3
shun <ledhouse2077@gmail.com>
2024-02-11 12:15:47 +0900

    7.1 画面レイアウトの実装 2024/02/11 実装完了

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -12,6 +13,13 @@ public class LoginController {
 	public String getLogin() {
 		return "login/login";
 		//return "login/yahoo";
+	}
+	
+	
+	/* ユーザー一覧画面にリダイレクト */
+	@PostMapping("/login")
+	public String postLogin() {
+		return "redirect:/user/list";
 	}
 	
 }
